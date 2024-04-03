bc4738c5b282be88938827cde2c2a09fb6a08cc8
leonardo <leotxsp@gmail.com>
2024-04-03 19:26:41 -0300

    Implementando event repository

--- src/models/entities/events.py
+++ src/models/entities/events.py
@@ -1,10 +1,13 @@
-from src.models.settings.base import base
+from src.models.settings.base import Base
 from sqlalchemy import Column,String,Integer
 
-class Events(base):
+class Events(Base):
     __tablename__ = "events"
     id = Column(String, primary_key=True)
     title = Column(String, nullable=False)
-    datails = Column(String)
+    details = Column(String)
     slug = Column(String, nullable=False)
     maximum_attendees = Column(Integer)
+
+    def __repr__(self):
+        return f"Events [title={self.title}, maximum_attendees={self.maximum_attendees}]"
--- src/models/settings/connection.py
+++ src/models/settings/connection.py
@@ -1,14 +1,15 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
-class DBConnectionHandler:
+class __DBConnectionHandler:
     def __init__(self) -> None:
         self.__connection_string = "{}:///{}".format(
             "sqlite",
             "storage.db"
         )
         self.__engine = None 
-        self.__session = None
+        self.session = None
 
     def connect_to_db(self) -> None:
         self.__engine = create_engine(self.__connection_string)
@@ -17,7 +18,10 @@ class DBConnectionHandler:
         return self.__engine
     
     def __enter__(self):
-        print("olha eu aqui")
+        session_maker = sessionmaker()
+        self.session = session_maker(bind=self.__engine)
+        return self
     def __exit__(self, exc_type, exc_val, exc_tb):
-        print ("estou saindo")
+        self.session.close()
         
+db_connection_handler = __DBConnectionHandler()
